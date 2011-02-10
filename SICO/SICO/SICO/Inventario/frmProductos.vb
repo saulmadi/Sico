6d00ccd1e197d11f5758f8dd8f126d6d8e765a0f
Saúl Antonio Mayorquin Diaz <saulmadi@gmailcom>
2011-02-10 07:54:05 +0000

    e

--- SICO/SICO/SICO/Inventario/frmProductos.vb
+++ SICO/SICO/SICO/Inventario/frmProductos.vb
@@ -1,6 +1,16 @@
 ﻿Imports SICO.ctrla
+Imports SICO.lgla
+Imports SICO.lgla2
 Public Class frmProductos
-
+    Private _Producto As New Productos
+    Public Property Producto() As Productos
+        Get
+            Return _Producto
+        End Get
+        Set(ByVal value As Productos)
+            _Producto = value
+        End Set
+    End Property
     Private Sub frmProductos_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         CrtImagen1.DialagoArchivo.InitialDirectory = My.Computer.FileSystem.SpecialDirectories.MyPictures
         Grid1.DarFormato("id", "Código", True)
@@ -8,5 +18,6 @@ Public Class frmProductos
         Grid1.DarFormato("fecha", "Fecha de última compra", True)
     End Sub
 
+    
     
 End Class
